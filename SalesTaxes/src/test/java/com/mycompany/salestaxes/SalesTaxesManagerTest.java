77a88c62ac1f15194e5567c8e62faa32dd096375
ndr86 <a.grimaldi86@gmail.com>
2017-06-14 10:47:37 +0200

    Aggiustati test case

--- SalesTaxes/src/test/java/com/mycompany/salestaxes/SalesTaxesManagerTest.java
+++ SalesTaxes/src/test/java/com/mycompany/salestaxes/SalesTaxesManagerTest.java
@@ -37,7 +37,13 @@ public class SalesTaxesManagerTest {
     }
 
     /**
-     * TEST CASE: Input 1
+     * TEST CASE:
+     *
+     * <br><br>Input 1:<br>1 book at 12.49<br>1 music CD at 14.99<br>1 chocolate bar at 0.85
+     *
+     * <br><br>Output 1:<br>1 book : 12.49<br>1 music CD: 16.49<br>1 chocolate bar: 0.85<br>Sales Taxes: 1.50<br>Total:
+     * 29.83
+     *
      */
     @Test
     public void input1() {
@@ -75,7 +81,12 @@ public class SalesTaxesManagerTest {
     }
 
     /**
-     * TEST CASE: Input 2
+     * TEST CASE:
+     *
+     * <br><br>Input 2:<br>1 imported box of chocolates at 10.00<br>1 imported bottle of perfume at 47.50
+     *
+     * <br><br>Output 2:<br>1 imported box of chocolates: 10.50<br>1 imported bottle of perfume: 54.65<br>Sales Taxes:
+     * 7.65<br>Total: 65.15
      */
     @Test
     public void input2() {
@@ -107,7 +118,13 @@ public class SalesTaxesManagerTest {
     }
 
     /**
-     * TEST CASE: Input 3
+     * TEST CASE:
+     *
+     * <br><br>Input 3:<br>1 imported bottle of perfume at 27.99<br>1 bottle of perfume at 18.99<br>1 packet of headache
+     * pills at 9.75<br>1 box of imported chocolates at 11.25
+     *
+     * <br><br>Output 3:<br>1 imported bottle of perfume: 32.19<br>1 bottle of perfume: 20.89<br>1 packet of headache
+     * pills: 9.75<br>1 imported box of chocolates: 11.85<br>Sales Taxes: 6.70<br>Total: 74.68
      */
     @Test
     public void input3() {
